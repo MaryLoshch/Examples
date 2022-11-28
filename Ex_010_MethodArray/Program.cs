1e816e45fe1326166a87e9e0d66da1859c7bf927
мария <oops.mari@mail.ru>
2022-11-28 17:31:19 +0300

    Сохраняю пример 10

---
+++ Ex_010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 26, 8, 73, 34, 315, 64, 7, 8 };
+int N = array.Length;
+int find = 8;
+
+int index = 0;
+while (index < N)
+{
+    
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
